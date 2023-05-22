cc9cccc7fe0674828a319c04fd4d03d04ee4f413
hshimizu <hisatosi35350@gmail.com>
2023-05-22 12:10:41 +0900

    やっぱりこっちかな

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 21:08:02 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/05/22 11:29:04 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/05/22 12:08:56 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,9 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	if (lst)
+	if (lst && del)
 	{
-		if (del)
-			del(lst->content);
+		del(lst->content);
 		free(lst);
 	}
 }
