593afbe8c62f8014c76edea2cf1b226ffe7258e2
hshimizu <hisatosi35350@gmail.com>
2023-05-26 15:38:02 +0900

    a

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 14:23:55 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/05/22 17:30:15 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/05/26 15:35:38 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	p2 = (const unsigned char *)s2;
 	while (n--)
 		if (*p1++ != *p2++)
-			return ((int)*--p1 - *--p2);
+			return ((int)(*--p1 - *--p2));
 	return (0);
 }
