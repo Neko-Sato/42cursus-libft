aa23b9dbcf61781a0291414775379af1b0c418bd
Neko-Sato <hisatosi35350@gmail.com>
2023-10-19 19:26:57 +0900

    norm

--- ft_vector/ft_vector_reserve.c
+++ ft_vector/ft_vector_reserve.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 23:39:20 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/10/19 18:46:25 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/10/19 19:26:42 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ int	ft_vector_reserve(void *vector_ptr, size_t capacity)
 		return (0);
 	old_vector = *(void **)vector_ptr;
 	new_vector = ft_vector(
-		old_vector,
-		ft_vector_size(old_vector),
-		capacity);
+			old_vector,
+			ft_vector_size(old_vector),
+			capacity);
 	if (!new_vector)
 		return (-1);
 	ft_vector_del(old_vector);
