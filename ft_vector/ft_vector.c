3e1c5a1cac56802126b9d885c0a6fd2e973f6ce5
Neko-Sato <hisatosi35350@gmail.com>
2023-10-20 02:03:05 +0900

    ミス

--- ft_vector/ft_vector.c
+++ ft_vector/ft_vector.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 22:43:07 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/10/19 18:19:41 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/10/20 02:02:47 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	*ft_vector(void *array, size_t size, size_t capacity)
 
 	if (capacity < size)
 		capacity = size;
-	vector = malloc(sizeof(size_t[3]) + capacity);
+	vector = malloc(sizeof(size_t[2]) + capacity);
 	if (!vector)
 		return (NULL);
 	vector = &((size_t *)vector)[2];
