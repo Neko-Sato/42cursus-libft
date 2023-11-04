f401905a6685fb500dd8a677151ed3c304de6c76
Neko-Sato <hisatosi35350@gmail.com>
2023-11-04 19:42:09 +0900

    ft_isnumber issigned

--- ft_utils/ft_isnumber.c
+++ ft_utils/ft_isnumber.c
@@ -6,16 +6,16 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 02:49:40 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/10/11 00:47:43 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/11/04 19:41:08 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_string.h>
 #include <ft_ctype.h>
 
-int	ft_isnumber(const char *src)
+int	ft_isnumber(const char *src, int issigned)
 {
-	if (*src && ft_strchr("+-", *src))
+	if (issigned && *src && ft_strchr("+-", *src))
 		src++;
 	if (!*src)
 		return (0);
