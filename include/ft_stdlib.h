df23e100fe03b1f03a37b77ae5f4e092a1f7a46f
Neko-Sato <hisatosi35350@gmail.com>
2023-11-26 22:30:39 +0900

    del realloc

--- include/ft_stdlib.h
+++ include/ft_stdlib.h
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 22:30:19 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/11/26 12:53:11 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/11/26 22:29:48 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ long			ft_strtol(const char *nptr, char **endptr, int base);
 int				ft_atoi(const char *str);
 long			ft_atol(const char *str);
 void			*ft_calloc(size_t count, size_t size);
-void			*ft_realloc(void *ptr, size_t size);
 char			*ft_itoa(int n);
 char			*ft_utoa(unsigned int n);
 
