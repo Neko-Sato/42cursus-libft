ff08144dbad874ff5e3380114016a72493289002
Neko-Sato <hisatosi35350@gmail.com>
2023-12-11 23:50:54 +0900

    ft_switchcase

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/06/22 21:58:50 by hshimizu          #+#    #+#              #
-#    Updated: 2023/12/09 03:45:43 by hshimizu         ###   ########.fr        #
+#    Updated: 2023/12/11 23:47:46 by hshimizu         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -129,6 +129,7 @@ SRCS 		= \
 			ft_gcd.c \
 			ft_lcm.c \
 			ft_isprime.c \
+			ft_switchcase.c \
 		) \
 		$(addprefix ft_xlst/, \
 			ft_xlst2array.c \
--- include/ft_utils.h
+++ include/ft_utils.h
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 22:30:19 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/11/07 20:24:12 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/12/11 23:48:55 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,21 @@
 
 # include <stddef.h>
 
-int		ft_sign(int n);
-int		ft_max(int a, int b);
-int		ft_min(int a, int b);
-size_t	ft_digit(unsigned int n);
-int		ft_isnumber(const char *src, int issigned);
-int		ft_gcd(int a, int b);
-int		ft_lcm(int a, int b);
-int		ft_isprime(int n);
+typedef struct s_switchcase_args
+{
+	size_t	(*get_case_num)(void *args);
+	size_t	case_max;
+	int		(**case_fns)(void *args);
+	void	*args;
+}			t_switchcase_args;
+
+int			ft_sign(int n);
+int			ft_max(int a, int b);
+int			ft_min(int a, int b);
+size_t		ft_digit(unsigned int n);
+int			ft_isnumber(const char *src, int issigned);
+int			ft_gcd(int a, int b);
+int			ft_lcm(int a, int b);
+int			ft_isprime(int n);
 
 #endif
