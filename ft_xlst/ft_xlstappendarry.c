bea03d66a6584e6f863766e93400af10f1c19863
Neko-Sato <hisatosi35350@gmail.com>
2024-03-01 02:06:28 +0900

    update

--- ft_xlst/ft_xlstappendarry.c
+++ ft_xlst/ft_xlstappendarry.c
@@ -6,27 +6,28 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 14:47:32 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/02/29 04:54:20 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/03/01 02:00:44 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_xlst.h>
 
-int	ft_xlstappendarry(t_xlst **lst_ptr, void **arry, size_t size)
+int	ft_xlstappendarry(t_xlst **lst_ptr, void *arry, size_t len, size_t size)
 {
 	t_xlst	**last;
 	t_xlst	*temp;
 
-	if (!arry)
+	if (!arry || !len)
 		return (0);
 	temp = NULL;
-	while (*arry)
+	while (len--)
 	{
-		if (ft_xlstappend(lst_ptr, *arry++, size))
+		if (ft_xlstappend(lst_ptr, arry, size))
 		{
 			ft_xlstclear(&temp);
 			return (-1);
 		}
+		arry = (char *)arry + size;
 	}
 	last = lst_ptr;
 	while (*last)
