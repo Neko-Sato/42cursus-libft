05644430bab675ae065917c6081542f36aedde74
Neko-Sato <hisatosi35350@gmail.com>
2024-02-29 20:38:43 +0900

    move putstrarry_fd

--- include/ft_put_fd.h
+++ include/ft_put_fd.h
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 22:30:19 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/10/10 23:07:34 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/02/29 20:33:08 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+
+void	ft_putstrarry_fd(char *ss[], char *delimiter, int fd);
 
 #endif
--- include/ft_utils.h
+++ include/ft_utils.h
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 22:30:19 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/02/29 07:53:47 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/02/29 20:33:15 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,5 @@ int		ft_intcmp(int a, int b);
 int		ft_intptrcmp(void *a, void *b);
 int		ft_strptrcmp(void *a, void *b);
 int		ft_noop(void);
-void	ft_putstrarry_fd(char *ss[], char *delimiter, int fd);
 
 #endif
