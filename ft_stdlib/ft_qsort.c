6de800735a48535a60d8a3fb5b07cd53bcc369b6
Neko-Sato <hisatosi35350@gmail.com>
2024-02-29 21:02:05 +0900

    update qsort

--- ft_stdlib/ft_qsort.c
+++ ft_stdlib/ft_qsort.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 20:27:25 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/02/29 20:46:01 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/02/29 21:00:56 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,27 +46,21 @@ static void	case_pair(void *base, size_t nmemb, size_t size, t_compar_fn compar)
 static size_t	partition(void *base, size_t nmemb, size_t size,
 		t_compar_fn compar)
 {
-	size_t	pivot;
-	size_t	i[2];
-	void	*obj[3];
+	void	*current;
+	void	*pivot;
+	size_t	i;
+	size_t	j;
 
-	pivot = nmemb / 2;
-	i[0] = 0;
-	i[1] = nmemb - 1;
-	while (i[0] <= i[1])
+	pivot = (char *)base + size * (nmemb - 1);
+	i = 0;
+	j = 0;
+	while (j < nmemb - 1)
 	{
-		obj[0] = (char *)base + pivot * size;
-		obj[1] = (char *)base + i[0] * size;
-		obj[2] = (char *)base + i[1] * size;
-		if (pivot != i[0] && compar(obj[1], obj[0]) > 0)
-		{
-			ft_memswap(obj[1], obj[2], size);
-			if (pivot == i[1])
-				pivot = i[0];
-			i[1]--;
-		}
-		else
-			i[0]++;
+		current = (char *)base + size * j;
+		if (compar(current, pivot) <= 0)
+			ft_memswap(current, (char *)base + size * i++, size);
+		j++;
 	}
-	return (pivot);
+	ft_memswap((char *)base + size * i, pivot, size);
+	return (i);
 }
