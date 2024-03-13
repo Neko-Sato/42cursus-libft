8ba53848dd4e637140e5fa22e470b7d87f2ca6bc
Neko-Sato <hisatosi35350@gmail.com>
2024-03-14 00:27:28 +0900

    update

--- ft_stdlib/ft_itoa.c
+++ ft_stdlib/ft_itoa.c
@@ -6,34 +6,29 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 17:08:04 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/10/11 00:07:15 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/03/13 21:44:00 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_string.h>
-#include <ft_stdlib.h>
-#include <stddef.h>
-#include <stdlib.h>
 
 char	*ft_itoa(int n)
 {
-	char			buff[12];
-	char			*head;
+	char			buf[12];
+	int				i;
 	unsigned int	m;
 
-	if (!n)
-		return (ft_strdup("0"));
-	head = &buff[11];
-	*head-- = '\0';
+	i = 12;
+	buf[--i] = '\0';
 	m = ft_abs(n);
 	while (1)
 	{
-		*head-- = '0' + m % 10;
+		buf[--i] = '0' + m % 10;
 		m /= 10;
 		if (!m)
 			break ;
 	}
 	if (n < 0)
-		*head-- = '-';
-	return (ft_strdup(++head));
+		buf[--i] = '-';
+	return (ft_strdup(&buf[i]));
 }
