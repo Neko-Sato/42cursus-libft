9b6db8b2f19b0badd784708525a678d5c2183a82
Neko-Sato <hisatosi35350@gmail.com>
2024-03-13 18:19:40 +0900

    update

--- ft_put_fd/ft_putnbr_fd.c
+++ ft_put_fd/ft_putnbr_fd.c
@@ -6,28 +6,12 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 19:23:19 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/10/11 00:47:09 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/03/13 18:19:14 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_put_fd.h>
 #include <ft_stdlib.h>
-
-static size_t	ft_itoalen(int n)
-{
-	size_t			len;
-	unsigned int	m;
-
-	if (!n)
-		return (1);
-	len = 0;
-	if (n < 0)
-		len++;
-	m = ft_abs(n);
-	while (m)
-		m /= 10 + 0 * len++;
-	return (len);
-}
 
 void	ft_putnbr_fd(int n, int fd)
 {
@@ -35,17 +19,17 @@ void	ft_putnbr_fd(int n, int fd)
 	int				i;
 	unsigned int	m;
 
-	i = ft_itoalen(n);
-	buf[i--] = '\0';
+	i = 12;
+	buf[--i] = '\0';
 	m = ft_abs(n);
 	while (1)
 	{
-		buf[i--] = '0' + m % 10;
+		buf[--i] = '0' + m % 10;
 		m /= 10;
 		if (!m)
 			break ;
 	}
 	if (n < 0)
-		buf[i--] = '-';
-	ft_putstr_fd(buf, fd);
+		buf[--i] = '-';
+	ft_putstr_fd(&buf[i], fd);
 }
