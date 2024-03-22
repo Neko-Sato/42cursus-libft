b3f1270fd329e1d4e43665f1926306f79b3521c1
Neko-Sato <hisatosi35350@gmail.com>
2024-03-22 23:55:23 +0900

    update

--- ft_string/ft_strndup.c
+++ ft_string/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 10:38:26 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/02/24 13:56:13 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/03/22 23:55:00 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ char	*ft_strndup(const char *s1, size_t n)
 	char	*s2;
 	size_t	size;
 
-	size = ft_strlen(s1) + 1;
-	if (n < size)
-		size = n;
+	size = ft_strnlen(s1, n) + 1;
 	s2 = (char *)malloc(size * sizeof(char));
 	ft_strlcpy(s2, s1, size);
 	return (s2);
