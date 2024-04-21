202bd93533ce17391c697f5fb0837c0e242ae56c
Neko-Sato <hisatosi35350@gmail.com>
2024-04-22 04:15:22 +0900

    update fix strtol

--- ft_stdlib/ft_strtol.c
+++ ft_stdlib/ft_strtol.c
@@ -6,68 +6,83 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 10:15:45 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/04/22 01:56:17 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/04/22 03:50:39 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <errno.h>
 #include <ft_ctype.h>
+#include <stddef.h>
 
+static int	pre(const char **nptr, int *base, int *neg);
+static int	internal(const char **nptr, int neg, int base, unsigned long *acc);
 static int	tonbr(int c);
-static long	core(const char *nptr, int neg, char **endptr, int base);
 
 long	ft_strtol(const char *nptr, char **endptr, int base)
 {
-	int	neg;
+	int				neg;
+	unsigned long	acc;
 
-	neg = 0;
-	if (base < 1 || 36 < base)
+	if (pre(&nptr, &base, &neg))
 	{
 		errno = EINVAL;
+		*endptr = NULL;
 		return (0);
 	}
-	while (ft_isspace(*nptr))
-		nptr++;
-	if (*nptr == '+' || *nptr == '-')
-		neg = *nptr++ == '-';
-	if ((base == 0 || base == 16) && nptr[0] == '0'
-		&& ft_tolower(nptr[1]) == 'x')
+	if (internal(&nptr, neg, base, &acc))
 	{
-		base = 16;
-		nptr += 2;
+		errno = EINVAL;
+		acc = (~0ul >> 1) ^ -neg;
 	}
-	if (!base)
-		base = (int []){10, 8}[*nptr++ == '0'];
-	return (core(nptr, neg, endptr, base));
-}
-
-static long	core(const char *nptr, int neg, char **endptr, int base)
-{
-	unsigned long	acc;
-	unsigned long	cutoff;
-	int				n;
-
-	cutoff = (~0ul >> 1) ^ -neg;
-	acc = 0;
-	while (1)
-	{
-		n = tonbr(*nptr);
-		if (n < 0 || base <= n)
-			break ;
-		if (cutoff - acc < (unsigned long)n)
-		{
-			acc = cutoff;
-			errno = EINVAL;
-			break ;
-		}
-		acc = acc * base + n;
-		nptr++;
-	}
-	if (neg)
-		acc *= -1;
 	if (endptr)
 		*endptr = (char *)nptr;
 	return (acc);
+}
+
+static int	pre(const char **nptr, int *base, int *neg)
+{
+	*neg = 0;
+	while (ft_isspace(**nptr))
+		(*nptr)++;
+	if (**nptr == '+' || **nptr == '-')
+		*neg = *(*nptr)++ == '-';
+	if ((*base == 0 || *base == 16) && (*nptr)[0] == '0'
+		&& ft_tolower((*nptr)[1]) == 'x')
+	{
+		*base = 16;
+		*nptr += 2;
+	}
+	if (!*base)
+		*base = (int []){10, 8}[**nptr == '0'];
+	return (*base < 2 || 36 < *base);
+}
+
+static int	internal(const char **nptr, int neg, int base, unsigned long *acc)
+{
+	int				overflow;
+	unsigned long	cutoff;
+	int				cutlim;
+	int				n;
+
+	overflow = 0;
+	cutoff = (~0ul >> 1) ^ -neg;
+	cutlim = cutoff % base;
+	cutoff /= base;
+	*acc = 0;
+	while (**nptr)
+	{
+		n = tonbr(**nptr);
+		if (n < 0 || base <= n)
+			break ;
+		if (overflow || *acc > cutoff || (*acc == cutoff && n > cutlim))
+			overflow = 1;
+		else
+			*acc = *acc * base + n;
+		(*nptr)++;
+	}
+	if (!overflow && neg)
+		*acc = -*acc;
+	return (overflow);
 }
 
 static int	tonbr(int c)
