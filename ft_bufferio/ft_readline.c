e7a822116ecb26e6b69fe21b794a3235efeb2718
Neko-Sato <hisatosi35350@gmail.com>
2024-04-27 11:44:16 +0900

    update

--- ft_bufferio/ft_readline.c
+++ ft_bufferio/ft_readline.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 05:08:58 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/04/27 10:00:22 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/04/27 11:34:56 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,25 +35,24 @@ char	*ft_readline(t_bufferio *io)
 
 static int	read_until_line(t_bufferio *io, size_t *size)
 {
-	int ret;
-	char *enter;
-	size_t tmp;
+	int		ret;
+	char	*enter;
+	size_t	start;
 
-	tmp = 0;
+	ret = 0;
+	start = 0;
 	while (1)
 	{
-		enter = ft_strnchr(&io->_buf[tmp], '\n', io->_len - tmp);
-		if (enter)
+		enter = ft_strnchr(&io->_buf[io->_pos + start], '\n', io->_len - start);
+		if (enter || ret == 1)
 			break ;
-		tmp = io->_len;
+		start = io->_len;
 		ret = ft_bufferio_read(io, io->_capacity * 1.5);
 		if (ret == -1)
 			return (-1);
-		else if (ret == 1)
-			break ;
 	}
 	if (enter)
-		*size = enter - &io->_buf[io->_pos];
+		*size = &enter[1] - &io->_buf[io->_pos];
 	else
 		*size = io->_len;
 	return (0);
