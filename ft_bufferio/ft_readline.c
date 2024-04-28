787c2f284f9f316ff016a7c1d80b96f5eaf06f39
Neko-Sato <hisatosi35350@gmail.com>
2024-04-28 15:01:51 +0900

    update

--- ft_bufferio/ft_readline.c
+++ ft_bufferio/ft_readline.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 05:08:58 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/04/27 11:34:56 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/04/28 14:59:40 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,21 @@
 
 static int	read_until_line(t_bufferio *io, size_t *size);
 
-char	*ft_readline(t_bufferio *io)
+int	ft_readline(char **line, t_bufferio *io)
 {
-	char	*result;
-	int		ret;
 	size_t	size;
 
-	ret = read_until_line(io, &size);
-	if (ret == -1 || !io->_len)
-		return (NULL);
-	result = ft_strndup(&io->_buf[io->_pos], size);
-	if (!result)
-		return (NULL);
+	*line = NULL;
+	if (read_until_line(io, &size))
+		return (-1);
+	if (io->_len)
+		return (0);
+	*line = ft_strndup(&io->_buf[io->_pos], size);
+	if (!*line)
+		return (-1);
 	io->_pos += size;
 	io->_len -= size;
-	return (result);
+	return (0);
 }
 
 static int	read_until_line(t_bufferio *io, size_t *size)
--- include/ft_bufferio.h
+++ include/ft_bufferio.h
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 22:30:19 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/04/27 09:36:02 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/04/28 15:00:56 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,6 @@ void		ft_close(t_bufferio *io, int do_close);
 ssize_t		ft_read(t_bufferio *io, char *buf, size_t size);
 ssize_t		ft_write(t_bufferio *io, char *buf, size_t size);
 
-char		*ft_readline(t_bufferio *io);
+int			ft_readline(char **line, t_bufferio *io);
 
 #endif
