2df5a98a534b876a2fb22faad8d4045b8e1b57bb
Neko-Sato <hisatosi35350@gmail.com>
2024-05-02 06:00:04 +0900

    update

--- ft_bufferio/ft_readline.c
+++ ft_bufferio/ft_readline.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 05:08:58 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/04/28 14:59:40 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/05/02 05:59:02 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_readline(char **line, t_bufferio *io)
 	*line = NULL;
 	if (read_until_line(io, &size))
 		return (-1);
-	if (io->_len)
+	if (!io->_len)
 		return (0);
 	*line = ft_strndup(&io->_buf[io->_pos], size);
 	if (!*line)
