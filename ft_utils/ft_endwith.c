3ee966e5af5c601b261488ae81b2371242b211ae
Neko-Sato <hisatosi35350@gmail.com>
2024-05-02 05:27:06 +0900

    update

--- ft_utils/ft_endwith.c
+++ ft_utils/ft_endwith.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 05:01:03 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/05/02 05:10:28 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/05/02 05:24:02 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_endwith(char *str, char *suffix)
 
 	slen = ft_strlen(str);
 	len = ft_strlen(suffix);
-	if (len < slen)
+	if (slen < len)
 		return (0);
 	return (!ft_strcmp(str + slen - len, suffix));
 }
