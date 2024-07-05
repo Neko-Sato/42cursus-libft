ef8a77b77a2934b2d0bf97374823d83f7d769bd9
Neko-Sato <hisatosi35350@gmail.com>
2024-07-06 01:14:50 +0900

    update

--- ft_color/ft_color_add.c
+++ ft_color/ft_color_add.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 13:07:38 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/06/23 22:33:53 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/07/05 17:13:46 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,7 @@ t_color	ft_color_add(t_color a, t_color b)
 	const double	a_alpha = a._.alpha / 0xffp0;
 	const double	b_alpha = b._.alpha / 0xffp0;
 
-	color.raw = 0;
-	color._.alpha = 0xff * (a_alpha + b_alpha - a_alpha * b_alpha);
+	color._.alpha = 0xff * (a_alpha + b_alpha * (1 - a_alpha));
 	color._.red = ft_min(0xff, a._.red * a_alpha + b._.red * b_alpha);
 	color._.green = ft_min(0xff, a._.green * a_alpha + b._.green * b_alpha);
 	color._.blue = ft_min(0xff, a._.blue * a_alpha + b._.blue * b_alpha);
--- ft_color/ft_color_sub.c
+++ ft_color/ft_color_sub.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 13:07:38 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/06/23 23:05:27 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/07/05 17:13:43 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,9 @@ t_color	ft_color_sub(t_color a, t_color b)
 	const double	a_alpha = a._.alpha / 0xffp0;
 	const double	b_alpha = b._.alpha / 0xffp0;
 
-	color.raw = 0;
-	color._.alpha = 0xff * (a_alpha + b_alpha - a_alpha * b_alpha);
-	color._.red = (0xff - (0xff - a._.red) * a_alpha) * (0xff - (0xff - b._.red)
-			* b_alpha) / 0xffp0;
-	color._.green = (0xff - (0xff - a._.green) * a_alpha) * (0xff - (0xff
-				- b._.green) * b_alpha) / 0xffp0;
-	color._.blue = (0xff - (0xff - a._.blue) * a_alpha) * (0xff - (0xff
-				- b._.blue) * b_alpha) / 0xffp0;
+	color._.alpha = 0xff * a_alpha * b_alpha;
+	color._.red = a._.red * b._.red / 0xffp0;
+	color._.green = a._.green * b._.green / 0xffp0;
+	color._.blue = a._.blue * b._.blue / 0xffp0;
 	return (color);
 }
