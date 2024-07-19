64c55b31cc5569b2edcd04b3bacac1494f17f1b5
Neko-Sato <hisatosi35350@gmail.com>
2024-07-19 20:50:47 +0900

    update

--- ft_color/ft_color_brightness.c
+++ ft_color/ft_color_brightness.c
@@ -6,19 +6,20 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 17:37:33 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/07/13 17:39:40 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/07/14 05:51:41 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_color.h>
+#include <ft_utils.h>
 
 t_color	ft_color_brightness(double brightness, t_color color)
 {
 	const t_color	tmp = {._ = {
 		.alpha = color._.alpha,
-		.red = color._.red * brightness,
-		.green = color._.green * brightness,
-		.blue = color._.blue * brightness
+		.red = ft_fmin(0xff, color._.red * brightness),
+		.green = ft_fmin(0xff, color._.green * brightness),
+		.blue = ft_fmin(0xff, color._.blue * brightness),
 	}};
 
 	return (tmp);
