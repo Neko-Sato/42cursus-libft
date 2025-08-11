db06638afa4d55b4a77069ee2bd8bdd858b73456
Uru Aki <uruaki@tsuki-2.local>
2025-08-11 16:56:32 +0900

    update

--- ft_btree/ft_btree_internal.h
+++ ft_btree/ft_btree_internal.h
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@42tokyo.student.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 06:08:37 by hshimizu          #+#    #+#             */
-/*   Updated: 2025/08/11 11:22:08 by hshimizu         ###   ########.fr       */
+/*   Updated: 2025/08/11 16:56:22 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,5 @@
 # define FT_BTREE_INTERNAL_H
 
 # include <ft_btree/ft_btree.h>
-
-t_btree_node	*__ft_btree_rotate_left(t_btree_node *node);
-t_btree_node	*__ft_btree_rotate_right(t_btree_node *node);
-t_btree_node	*__ft_btree_rotate_left_right(t_btree_node *node);
-t_btree_node	*__ft_btree_rotate_right_left(t_btree_node *node);
 
 #endif
