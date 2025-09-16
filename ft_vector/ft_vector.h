7ee9392727e1e92c1f666982f9f17c2ef2e64ced
Uru Aki <uruaki@tsuki-2.local>
2025-09-16 17:19:03 +0900

    update

--- ft_vector/ft_vector.h
+++ ft_vector/ft_vector.h
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@42tokyo.student.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 05:24:49 by hshimizu          #+#    #+#             */
-/*   Updated: 2025/09/11 20:39:26 by hshimizu         ###   ########.fr       */
+/*   Updated: 2025/09/16 17:18:40 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,5 @@ void		ft_vector_move(t_vector *dst, t_vector *src);
 void		*ft_vector_data(t_vector *vector);
 size_t		ft_vector_size(t_vector *vector);
 int			ft_vector_resize(t_vector *vector, size_t n);
-int			ft_vector_push_back(t_vector *vector, void *elem);
 
 #endif
