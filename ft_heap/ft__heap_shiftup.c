dc8e2c13bf7116c590ef09c97482d1633386714e
Uru Aki <uruaki@tsuki-2.local>
2025-11-03 14:24:47 +0900

    update

--- ft_heap/ft__heap_shiftup.c
+++ ft_heap/ft__heap_shiftup.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@42tokyo.student.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 02:31:19 by hshimizu          #+#    #+#             */
-/*   Updated: 2025/11/03 13:27:47 by hshimizu         ###   ########.fr       */
+/*   Updated: 2025/11/03 13:56:01 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,16 @@
 
 void	ft__heap_shiftup(t_heap_args *args, size_t index)
 {
-	size_t	parent;
+	void	*parent;
+	void	*child;
 
 	while (index)
 	{
-		parent = (index - 1) / 2;
-		if (args->compar(
-				(char *)args->base + args->size * parent,
-				(char *)args->base + args->size * index) >= 0)
+		child = (char *)args->base + args->size * index;
+		index = (index - 1) / 2;
+		parent = (char *)args->base + args->size * index;
+		if (args->compar(parent, child) >= 0)
 			break ;
-		ft__heap_swap(args,
-			(char *)args->base + args->size * parent,
-			(char *)args->base + args->size * index);
-		index = parent;
+		ft__heap_swap(args, parent, child);
 	}
 }
